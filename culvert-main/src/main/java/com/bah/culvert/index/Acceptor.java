6109e9e8f3bacfe8214d52c9be76075f6af981f6
Jesse Yates <jesse.k.yates@gmail.com>
2012-01-05 16:21:32 -0800

    Adding an acceptor for indexes.
    This is a better abstraction mechanism that we can leverage for determining if
    and index should accept a given key/value. Currently, we do it based on
    matching the cf and cq, but not all indexes will leverage the same comparison.

---
+++ culvert-main/src/main/java/com/bah/culvert/index/Acceptor.java
@@ -0,0 +1,24 @@
+package com.bah.culvert.index;
+
+import com.bah.culvert.data.CKeyValue;
+import com.bah.culvert.util.BaseConfigurable;
+import com.bah.culvert.transactions.Put;
+
+/**
+ * Determine if a {@link CKeyValue} is accepted by an {@link Index} to be
+ * indexed.
+ */
+public abstract class Acceptor extends BaseConfigurable {
+
+  /**
+   * Accept or reject a given key/value for indexing. After accepting a
+   * {@link CKeyValue}, that object and all other accepted {@link CKeyValue
+   * CKeyValues} will be passed as a {@link Put} to {@link Index#handlePut(Put)}
+   * @param kv key/value to be indexed
+   * @return <tt>true</tt> if the key/value should be passed to the index,
+   *         <tt>false</tt> otherwise
+   */
+  public abstract boolean accept(CKeyValue kv);
+  
+
+}
